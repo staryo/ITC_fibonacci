cd00a35121ec5cc8420467a28a3da42264cbae67
Артем Столов <artem.stolov@gmail.com>
2023-08-13 18:55:32 +0300

    added check of negative and not integer numbers

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,25 +1,33 @@
-const inputNumber = document.getElementById('inputNumber')
-const resultValue = document.getElementById('result')
-let cache = {1: 1, 2: 1}
+const inputNumber = document.getElementById('inputNumber');
+const resultValue = document.getElementById('result');
+let cache = {1: 1, 2: 1};
 
 function readInput() {
-    const number = inputNumber.value
-    let result = fibonacci(number)
-    if (result > 1_000_000) {
-        result = result.toExponential(4)    
+    const number = Number(inputNumber.value);
+    if (!Number.isInteger(number)) {
+        resultValue.textContent = 'Not an integer number';
+        return
     }
-    resultValue.textContent = result
-    inputNumber.value = number
+    if (number <= 0) {
+        resultValue.textContent = 'Not positive number';
+        return
+    }
+    let result = fibonacci(number);
+    if (result > 1_000_000) {
+        result = result.toExponential(4);
+    }
+    resultValue.textContent = result;
+    inputNumber.value = number;
 }
 
 function fibonacci(number) {
     if (number in cache) {
-        return cache[number]
+        return cache[number];
     } else {
-        cache[number] = fibonacci(number - 1) + fibonacci(number - 2)
-        return cache[number]
+        cache[number] = fibonacci(number - 1) + fibonacci(number - 2);
+        return cache[number];
     }
 }
 
-console.log(inputNumber.value)
+console.log(inputNumber.value);
 inputNumber.addEventListener('change', readInput);
